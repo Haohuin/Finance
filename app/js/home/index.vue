5b5be9a7960a6dabafba1142ef12ad98ff10a166
WangJw <huins.null@gmail.com>
2023-06-18 11:03:43 +0800

    Initial commit

---
+++ app/js/home/index.vue
@@ -0,0 +1,22 @@
+<template lang="html">
+  <div>
+    <div :class="$style.home">
+      <h1>Home</h1>
+    </div>
+    <div>
+      设计相关的内容,teetret
+    </div>
+  </div>
+</template>
+<script>
+export default {
+}
+</script>
+
+<style lang="scss" module>
+  @import '../../css/reset.scss';
+  .home{
+    color: red;
+    font-size: 80px;
+  }
+</style>
